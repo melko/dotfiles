fb821f0410def4179f8e4c972c8a06b30d62af8d
Melko <melko@frugalware.org>
2024-10-16 02:26:53 +0200

    lazyvim/nvim-cmp: overwrite default mapping since it uses <C-y>
    <C-y> and <C-e> are useful to clone text from the line above/below, so
    do not use it for selecting/aborting a completion.

--- lazyvim/lua/plugins/custom.lua
+++ lazyvim/lua/plugins/custom.lua
@@ -13,14 +13,32 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    -- disable inlay hints (function argument name showing) since they visually clutter everything
     opts = { inlay_hints = { enabled = false } },
   },
   {
     "hrsh7th/nvim-cmp",
-    opts = {
-      experimental = {
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.experimental = {
         ghost_text = false, -- disable annoying ghost text when autocompleting
       }
-    },
+      -- overwrite default mapping to remove <C-y>
+      opts.mapping = cmp.mapping({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = LazyVim.cmp.confirm({ select = true }),
+        ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-CR>"] = function(fallback)
+          cmp.abort()
+          fallback()
+        end,
+      })
+    end,
   },
 }
