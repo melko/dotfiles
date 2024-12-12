67596de006a37f2f6966ccdc4ca905c29ef5216d
Melko <melko@frugalware.org>
2024-12-12 01:13:43 +0100

    lazyvim: move autocmd definition from options.lua to autocmds.lua
    Since there is a specific file for autocmds, just use it.

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -1,3 +1,21 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- save position when switching buffer so it can be restored when entering it again
+vim.api.nvim_create_augroup('SaveWindowViewGroup', { clear = true })
+vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+  group = 'SaveWindowViewGroup',
+  pattern = '*',
+  callback = function ()
+    if vim.b.winview
+    then vim.fn.winrestview(vim.b.winview)
+    end
+    --else vim.b.winview = nil end
+  end,
+})
+vim.api.nvim_create_autocmd({ 'BufWinLeave' }, {
+  group = 'SaveWindowViewGroup',
+  pattern = '*',
+  callback = function () vim.b.winview = vim.fn.winsaveview() end,
+})
